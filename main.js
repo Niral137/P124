d93221d43a046da48eea4b9a66e1157736d32fa4
Niral137 <88650779+Niral137@users.noreply.github.com>
2023-06-29 20:39:45 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas= createCanvas(500,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " +noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw() {
+    background('#e9a8ff');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the Square will be: " + difference + "px";
+    fill('#820101')
+    stroke('#ff9696')
+    square(noseX, noseY, difference);
+}
